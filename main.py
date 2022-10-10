a9c8cbf1ffb586f79748788a8b3c96a9f0a4cf79
Richard Li <lichard49@gmail.com>
2022-10-10 14:34:27 -0400

    Added routing to index and sonar_demo; set up templates for each of those pages

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+templates = Jinja2Templates(directory='templates')
 
 
 @app.get('/')
-async def root():
-  return {'message': 'Hello World'}
+async def index(request: Request):
+  return templates.TemplateResponse('index.html', {'request': request})
+
+
+@app.get('/sonar_demo')
+async def sonar_demo(request: Request):
+  return templates.TemplateResponse('sonar_demo.html', {'request': request})
